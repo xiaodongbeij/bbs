4c9b6338b21646621b2f84b2231a45490ca47e4b
xiaodongbei <xiaodongbeij@gmail.com>
2020-02-28 11:05:10 +0000

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/about', 'StaticPagesController@about');
-Route::get('/help', 'StaticPagesController@help');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('/help', 'StaticPagesController@help')->name('help');
