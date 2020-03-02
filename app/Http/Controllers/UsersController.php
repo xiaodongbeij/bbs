d84a9a19e499d03eb7c230d624dddee905731ac8
xiaodongbei <xiaodongbeij@gmail.com>
2020-03-02 07:45:26 +0000

    邮箱激活

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class UsersController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth',[
-           'except' => ['show', 'create', 'store', 'index']
+           'except' => ['show', 'create', 'store', 'index', 'confirmEmail']
         ]);
         $this->middleware('guest', [
            'only' => 'create'
@@ -25,7 +26,7 @@ class UsersController extends Controller
     }
     public function create()
     {
-        return view('users.create_and_edit');
+        return view('users.create');
     }
 
     public function show(User $user)
@@ -46,9 +47,9 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password)
         ]);
-        Auth::login($user);
-        session()->flash('success', '欢迎，您将在这里开启一段新的历程');
-        return redirect()->route('users.show', [$user]);
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '请去注册邮箱中邮件进行激活。');
+        return redirect('/');
     }
 
     public function edit(User $user)
@@ -83,4 +84,36 @@ class UsersController extends Controller
         session()->flash('success', '成功删除用户！');
         return redirect()->back();
     }
+
+    /**
+     *  发送激活邮件
+     * @param $user
+     */
+    public function sendEmailConfirmationTo($user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'xiaoxiao@qq.com';
+        $name = 'xiaoxiao';
+        $to = $user->email;
+        $subject = '感谢注册 Weibo 应用! 请确认你的邮箱';
+        Mail::send($view,$data,function ($message)use($from,$name,$to,$subject){
+            $message->from($from,$name)->to($to)->subject($subject);
+        });
+    }
+
+    public function confirmEmail($token)
+    {
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '恭喜你，激活成功！');
+        return redirect()->route('users.show', [$user]);
+    }
+
+
 }
---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class UserObserver
+{
+    /**
+     * Handle the user "created" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function creating(User $user)
+    {
+        $user->activation_token = Str::random(10);
+    }
+
+    /**
+     * Handle the user "updated" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "restored" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the user "force deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
